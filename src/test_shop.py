7746e65577ef05532ae894e8c5a3b41bc0f1cfe9
Gergely Gáll <pryann@gmail.com>
2026-09-25 09:40:01 +0200

    add tests

--- src/test_shop.py
+++ src/test_shop.py
@@ -14,3 +14,29 @@ def test_gross_price_add_default_vat() -> None:
 
     # Assert
     assert result == 1270
+
+
+def test_gross_price_with_custom_vat() -> None:
+    # AAA pattern : Arrange, Act , Assert
+
+    # Arrange
+    net_price = 1000
+    vat_rate = 10
+
+    # Act
+    result = gross_price(net_price, vat_rate)
+
+    # Assert
+    assert result == 1100
+
+
+def test_gross_price_reject_negative_value_error() -> None:
+    # AAA pattern : Arrange, Act , Assert
+
+    # Arrange
+    net_price = -1000
+    vat_rate = 10
+
+    # Act - Assert
+    with pytest.raises(ValueError):
+        gross_price(net_price, vat_rate)
